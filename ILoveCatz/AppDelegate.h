cc481ceabccc7c88dbecfebd6f218896e2421b97
nathanchen <qche7764@uni.sydney.edu.au>
2015-03-17 16:43:07 +0800

    Initial Commit

---
+++ ILoveCatz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ILoveCatz
+//
+//  Created by Nate on 17/03/2015.
+//  Copyright (c) 2015 Nathan CHEN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ILoveCatz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ILoveCatz
+//
+//  Created by Nate on 17/03/2015.
+//  Copyright (c) 2015 Nathan CHEN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
